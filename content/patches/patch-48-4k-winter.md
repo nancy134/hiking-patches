403ec359eb4ce4d06c108a410266aadb8e984723
Nancy Piedra <nancy_piedra@yahoo.com>
2023-11-06 21:42:03 +0000

    48 4k winter

---
+++ content/patches/patch-48-4k-winter.md
@@ -0,0 +1,7 @@
+---
+title: NH 48 4000-ft peaks in winter 
+description: "To earn this patch, hike all New Hampshires 48 4k peaks in winter"
+image: "/images/patch-48-4k-winter.jpeg"
+date: 2022-01-08T06:00:00+00:00
+draft: false
+---
--- layouts/partials/Patches.js
+++ layouts/partials/Patches.js
@@ -7,43 +7,7 @@ const Patches = ({ posts }) => {
   const { patch_folder, summary_length } = config.settings;
   return (
     <div className="section row pb-0">
-      <div className="col-12 pb-12 lg:pb-24">
-        <div className="row items-center">
-          <div className="col-12 md:col-6">
-            {posts[0].frontmatter.image && (
-              <Image
-                className="h-auto w-full rounded-lg"
-                src={posts[0].frontmatter.image}
-                alt={posts[0].frontmatter.title}
-                width={540}
-                height={227}
-                priority={true}
-              />
-            )}
-          </div>
-          <div className="col-12 md:col-6">
-            <h2 className="h3 mb-2 mt-4">
-              <Link
-                href={`/${patch_folder}/${posts[0].slug}`}
-                className="block hover:text-primary"
-              >
-                {posts[0].frontmatter.title}
-              </Link>
-            </h2>
-            <p className="text-text">
-              {posts[0].frontmatter.description}
-            </p>
-            <Link
-              className="btn btn-primary mt-4"
-              href={`/${patch_folder}/${posts[0].slug}`}
-              rel=""
-            >
-              Read More
-            </Link>
-          </div>
-        </div>
-      </div>
-      {posts.slice(1).map((post, i) => (
+      {posts.map((post, i) => (
         <div key={`key-${i}`} className="col-12 mb-8 sm:col-6 lg:col-4">
           {post.frontmatter.image && (
             <Image
