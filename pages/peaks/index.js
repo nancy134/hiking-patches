9c5d3c8fe19886a86a0216e2ccb5d57ebd3f7103
Nancy Piedra <nancy_piedra@yahoo.com>
2023-11-29 23:08:02 +0000

    Add peak filtering

--- pages/peaks/index.js
+++ pages/peaks/index.js
@@ -1,4 +1,4 @@
-import BlogPagination, { getStaticProps } from "./page/[slug]";
+import PeakPagination, { getStaticProps } from "./page/[slug]";
 
 export { getStaticProps };
-export default BlogPagination;
+export default PeakPagination;
--- pages/peaks/page/[slug].js
+++ pages/peaks/page/[slug].js
@@ -5,6 +5,7 @@ import { getListPage, getSinglePage } from "@lib/contentParser";
 import { parseMDX } from "@lib/utils/mdxParser";
 import { markdownify } from "@lib/utils/textConverter";
 import Peaks from "@partials/Peaks";
+import Link from "next/link";
 const { peak_folder } = config.settings;
 
 // peak pagination
@@ -21,6 +22,30 @@ const PeakPagination = ({ postIndex, posts, currentPage, pagination }) => {
       <section className="section">
         <div className="container">
           {markdownify(title, "h1", "h1 text-center font-normal text-[56px]")}
+
+          <h4>Filters</h4>
+          <Link
+            href="/peaks"
+            className="cta-link inline-flex items-center text-primary mr-3"
+          >All</Link>
+          <Link
+            href="/peaks/page/newhampshire"
+            className="cta-link inline-flex items-center text-primary mr-3"
+          >New Hampshire</Link>
+          <Link
+            href="/peaks/page/maine"
+            className="cta-link inline-flex items-center text-primary mr-3"
+          >Maine</Link>
+          <Link
+            href="/peaks/page/vermont"
+            className="cta-link inline-flex items-center text-primary mr-3"
+          >Vermont</Link>
+          <Link
+            href="/peaks/page/massachusetts"
+            className="cta-link inline-flex items-center text-primary mr-3"
+          >Massachusetts</Link>
+
+
           <Peaks posts={currentPosts} />
           <Pagination
             section={peak_folder}
@@ -50,6 +75,12 @@ export const getStaticPaths = () => {
       },
     });
   }
+  // Filters
+  paths.push({params: {slug: 'newhampshire'}});
+  paths.push({params: {slug: 'vermont'}});
+  paths.push({params: {slug: 'newyork'}});
+  paths.push({params: {slug: 'maine'}});
+  paths.push({params: {slug: 'massachusetts'}});
 
   return {
     paths,
@@ -59,12 +90,31 @@ export const getStaticPaths = () => {
 
 // get peak pagination content
 export const getStaticProps = async ({ params }) => {
-  const currentPage = parseInt((params && params.slug) || 1);
-  const { pagination_peak } = config.settings;
-  const posts = getSinglePage(`content/${peak_folder}`).sort(
+
+  // State filters
+  var state = null; 
+  if (params && params.slug == "newhampshire") state = "New Hampshire";
+  if (params && params.slug == "vermont") state = "Vermont";
+  if (params && params.slug == "newyork") state = "New York";
+  if (params && params.slug == "maine") state = "Maine";
+  if (params && params.slug == "massachusetts") state = "Massachusetts";
+
+  var currentPage = 1;
+  if (!state) currentPage = parseInt((params && params.slug) || 1);
+
+  var { pagination_peak } = config.settings;
+
+  var filters = null;
+  if (state){
+    filters = { state: state}
+  }
+  const posts = getSinglePage(`content/${peak_folder}`,filters ).sort(
     (post1, post2) =>
       post2.frontmatter.elevation - post1.frontmatter.elevation
   );
+
+  if (state) pagination_peak = posts.length;
+
   const postIndex = await getListPage(`content/${peak_folder}/_index.md`);
   const mdxContent = await parseMDX(postIndex.content);
 
